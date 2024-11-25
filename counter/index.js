6450c8e056ffa66d7cde03dde1e9207a32fa07ea
Rizny-1708 <riznyofficial1708@gmail.com>
2024-11-25 10:34:56 +0530

    Add files via upload

---
+++ counter/index.js
@@ -0,0 +1,20 @@
+const dec = document.getElementById("dec");
+const reset = document.getElementById("reset");
+const inc = document.getElementById("inc");
+const countlabel = document.getElementById("countlabel");
+let count = 0;
+
+inc.onclick = function(){
+    count++;
+    countlabel.textContent=count;
+}
+
+dec.onclick = function(){
+    count--;
+    countlabel.textContent=count;
+}
+
+reset.onclick = function(){
+    count=0;
+    countlabel.textContent=count;
+}
